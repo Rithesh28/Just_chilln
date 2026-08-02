a8f483b5f63e53f0509d6fb0550501869755e2ef
Rithesh N D <ritheshnekraje@gmail.com>
2026-08-02 21:22:26 +0530

    Todays workdone java-patterns

--- java/patterns/upsidedownleftequi.java
+++ java/patterns/upsidedownleftequi.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 //import java.util.*;
-public class leftsideequi {
+public class upsidedownleftequi {
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
+        System.out.println("enther the rows:");
         int rows=sc.nextInt();
-        for(int i=1;i<=rows;i++){
+        for(int i=rows;i>=1;i--){
             for(int j=1;j<=rows-i;j++){
                 System.out.print(" ");
             }
-            for(int k=1;k<=i;k++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
-        System.out.println();
+            System.out.println();
         }
-
     }
 }
