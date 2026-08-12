762ef248ccdbac49d35b950e105193f905c8c5c0
Rithesh N D <ritheshnekraje@gmail.com>
2026-08-12 07:40:57 +0530

    java pattern solving

--- java/patterns/upsidedowntriangle.java
+++ java/patterns/upsidedowntriangle.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class triangle {
+public class upsidedowntriangle {
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the rows:");
         int rows=sc.nextInt();
-        for(int i=1;i<=rows;i++){
+        for(int i=rows;i>=1;i--){
             for(int j=1;j<=rows-i;j++){
                 System.out.print(" ");
             }
